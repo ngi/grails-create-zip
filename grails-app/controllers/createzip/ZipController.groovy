068241c8db2f7ed4b689aa09a319192be33ae375
Angelo Alfano <angeloalfano@gmail.com>
2010-12-14 10:44:20 +0100

    Support for immediate download

--- grails-app/controllers/createzip/ZipController.groovy
+++ grails-app/controllers/createzip/ZipController.groovy
@@ -6,8 +6,7 @@ class ZipController {
 	def directory = {
 		def directory = params['dir']
 		def filename = params['filename']
-		
-		response.setHeader('filename', filename)
+		response.setHeader("Content-disposition", "attachment; filename=\"" + filename + "\"");
 		response.setContentType("application/zip")
 		ZipFile.leftShift(response.outputStream, directory)
 		log.debug response
