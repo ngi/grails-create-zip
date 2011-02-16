c42d1494984d48163e0b8f8516f9f18736276426
Angelo Alfano <angeloalfano@gmail.com>
2011-02-16 15:14:05 +0100

    strip absolute dir

--- grails-app/controllers/createzip/ZipController.groovy
+++ grails-app/controllers/createzip/ZipController.groovy
@@ -6,9 +6,10 @@ class ZipController {
 	def directory = {
 		def directory = params['dir']
 		def filename = params['filename']
+		def stripdir = params['stripdir']?:""
 		response.setHeader("Content-disposition", "attachment; filename=\"" + filename + "\"");
 		response.setContentType("application/zip")
-		ZipFile.leftShift(response.outputStream, directory)
+		ZipFile.leftShift(response.outputStream, directory, stripdir)
 		log.debug response
 	}
 }
--- grails-app/taglib/createzip/CreateZipTagLib.groovy
+++ grails-app/taglib/createzip/CreateZipTagLib.groovy
@@ -5,8 +5,9 @@ class CreateZipTagLib {
 	def zipdirectory = { attrs, body ->
 		def dir = attrs.remove('dir')
 		def filename = attrs.remove('filename')
-		
+		def stripdir = attrs.remove('stripdir')
+
 		out << g.link(controller: "zip",
-			action: "directory",  params: [dir: dir, filename: filename])
+				action: "directory",  params: [dir: dir, filename: filename, stripdir: stripdir])
 	}
 }
